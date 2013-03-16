eac5745ad8052fba7eab57b42592defff6339a93
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-03-15 20:04:33 -0700

    Added spaces attribute

--- js/maze.js
+++ js/maze.js
@@ -9,6 +9,16 @@ function Maze(width, height) {
   this.startOrientation = null;
   this.endX             = null;
   this.endY             = null;
+
+  this.spaces = [];
+
+  var x, y;
+  for (x=1; x <= width; x += 1) {
+    this.spaces[x] = [];
+    for (y=1; y <= height; y += 1) {
+      this.spaces[x][y] = "(" + x + "," + y + ")";
+    }
+  }
 }
 
 Maze.prototype.setStart = function(x, y, orientation) {
