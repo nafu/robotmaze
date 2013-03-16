b1a83fa2dd83f8abd9f90220fa81b61615e5b2fc
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-03-15 19:58:35 -0700

    Added position attribute and some initialize functions

--- js/maze.js
+++ js/maze.js
@@ -1,6 +1,23 @@
 "use strict";
 
-function Maze() {
-  this.width = 7;
-  this.height = 5;
+function Maze(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.startX           = null;
+  this.startY           = null;
+  this.startOrientation = null;
+  this.endX             = null;
+  this.endY             = null;
+}
+
+Maze.prototype.setStart = function(x, y, orientation) {
+  this.startX = x;
+  this.startY = y;
+  this.startOrientation = orientation;
+}
+
+Maze.prototype.setEnd = function(x, y) {
+  this.endX = x;
+  this.endY = y;
 }
